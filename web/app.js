5dc3cdf71cc03021f617d1c213d151f49a529dd6
a.kombarov <a.kombarov@webbankir.com>
2026-02-02 21:22:41 +0300

    fix app

--- web/app.js
+++ web/app.js
@@ -1,7 +1,15 @@
 const tg = window.Telegram.WebApp;
+
+tg.ready();
 tg.expand();
 
 document.getElementById("createBtn").onclick = () => {
   const desc = document.getElementById("description").value;
-  alert("Буду оживлять: " + desc);
+
+  if (!desc) {
+    tg.showAlert("Напиши, кто это 🙂");
+    return;
+  }
+
+  tg.showAlert("Буду оживлять: " + desc);
 };
