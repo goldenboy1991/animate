aa97b3b392816cdc85d0c5b577f303a47c01d9cf
a.kombarov <a.kombarov@webbankir.com>
2026-02-04 14:59:35 +0300

    up

--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,12 @@ func main() {
 	http.HandleFunc("/api/generate-creature", GenerateCreature)
 
 	mux.HandleFunc("/feed", FeedHandler)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("PATH:", r.URL.Path)
+		http.NotFound(w, r)
+	})
+
 	log.Println("Backend startedd on :8080")
 	log.Fatal(http.ListenAndServe(":8080", withCORS(mux)))
 }
