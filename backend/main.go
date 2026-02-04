57a1b31db1c239d399c1ccc350f8f27f1e315569
a.kombarov <a.kombarov@webbankir.com>
2026-02-04 14:55:05 +0300

    up

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	http.HandleFunc("/api/generate-creature", GenerateCreature)
-	mux.HandleFunc("/feed", FeedHandler)
-	log.Println(os.Getenv("CF_ACCOUNT_ID"))
 
-	log.Println("Backend started on :8080")
+	mux.HandleFunc("/feed", FeedHandler)
+	log.Println("Backend startedd on :8080")
 	log.Fatal(http.ListenAndServe(":8080", withCORS(mux)))
 }
